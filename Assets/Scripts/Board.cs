c164aa33b6c31e5fc7a2b03765624bbc9f3d6980
Hao Asakura <kingofbandit27@gmail.com>
2022-04-27 23:37:40 +0200

    quit on eesc

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -63,6 +63,11 @@ public class Board : MonoBehaviour
         SpawnPiece();
     }
 
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
     public void SpawnPiece() {
         int random = nextPiece;
         nextPiece = Random.Range(0, tetrominoes.tetrominoesData.Length - 1);
